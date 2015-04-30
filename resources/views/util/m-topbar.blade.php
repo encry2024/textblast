a0a1dd4f9e9dde4c17ac2deb515cbe2f2091f51b
Jake <christan.jake2025@yahoo.com>
2015-04-30 16:18:45 +0800

    Update #22 - Added registration function. Changes on create_user_table migration (removed firstName and lastName column to fit the request of registrar class).

--- resources/views/util/m-topbar.blade.php
+++ resources/views/util/m-topbar.blade.php
@@ -9,7 +9,7 @@
             <ul class="nav navbar-nav navbar-right">
                 <li class="dropdown">
                     <a href="#" class="dropdown-toggle" data-toggle="dropdown" role="button" aria-expanded="false">
-                    Logged in as :: {!! Auth::user()->firstName !!} <span class="caret"></span></a>
+                    Logged in as :: {!! Auth::user()->name !!} <span class="caret"></span></a>
                     <ul class="dropdown-menu" role="menu">
                         <li><a href="{!! URL::to('/') !!}/auth/register"><span class="glyphicon glyphicon-user"></span> Register User</a></li>
                         <li><a href="{!! URL::to('/') !!}/history"><span class="glyphicon glyphicon-book"></span> History</a></li>
