cccb63275ab4ac31d8ec16d5d708745bfdfb063f
Jake <christan.jake2025@yahoo.com>
2015-04-30 15:29:05 +0800

    Update #19 - Added create_sms_table migration

---
+++ database/migrations/2015_04_24_151716_create_sms_table.php
@@ -0,0 +1,36 @@
+<?php
+
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class CreateSmsTable extends Migration {
+
+	/**
+	 * Run the migrations.
+	 *
+	 * @return void
+	 */
+	public function up()
+	{
+		Schema::create('sms', function(Blueprint $table) {
+			$table->increments('id');
+			$table->integer('recipient_id')->unsigned();
+			$table->integer('team_id')->unsigned();
+			$table->string('message');
+			$table->text('type');
+			$table->timestamps();
+			$table->softDeletes();
+		});
+	}
+
+	/**
+	 * Reverse the migrations.
+	 *
+	 * @return void
+	 */
+	public function down()
+	{
+		Schema::drop('sms');
+	}
+
+}
