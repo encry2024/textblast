fc6cc8e07a19795cca7c6946af4a89ef6897fd3d
Jake <christan.jake2025@yahoo.com>
2015-05-29 07:30:29 +0800

    Added template function, model, and migration

---
+++ app/Template.php
@@ -0,0 +1,9 @@
+<?php namespace App;
+
+use Illuminate\Database\Eloquent\Model as Eloquent;
+
+class Template extends Eloquent {
+
+	//
+	protected $fillable = ['name', 'view'];
+}
---
+++ database/migrations/2015_05_29_065246_add_view_column_in_templates_table.php
@@ -0,0 +1,30 @@
+<?php
+
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class AddViewColumnInTemplatesTable extends Migration {
+
+	/**
+	 * Run the migrations.
+	 *
+	 * @return void
+	 */
+	public function up()
+	{
+		Schema::table('templates', function($table){
+			$table->string('view')->after('name');
+		});
+	}
+
+	/**
+	 * Reverse the migrations.
+	 *
+	 * @return void
+	 */
+	public function down()
+	{
+		//
+	}
+
+}
