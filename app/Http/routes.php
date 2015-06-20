15b328c8455e44b4dc07565d3eb6301e169641a6
Jake <christan.jake2025@yahoo.com>
2015-06-20 14:13:11 +0800

    Added history page no function yet

--- app/Http/routes.php
+++ app/Http/routes.php
@@ -9,50 +9,53 @@ Route::bind('recipientNumber', 	function( $id ) 	{ return App\RecipientNumber::f
 Route::bind('template', 		function( $id )		{ return App\Template::find($id); });
 Route::bind('user',				function( $id )		{ return App\User::find($id); });
 
-# RESOURCES
-Route::resource('recipient', 'RecipientController', [ 'except' => ['create'] ]);
-Route::resource('recipientNumber', 'RecipientNumberController', [ 'only' => ['store','update','destroy'] ]);
-Route::resource('sms', 'SmsController', [ 'only'  =>  ['index', 'edit', 'store'], ]);
-Route::resource('team', 'TeamController');
-Route::resource('user', 'UserController');
-//Route::post('tag/recipient', ['as' => 'tr', 'uses' => 'RecipientTeamController@tag']);
-Route::post('delete/recipient', ['as' => 'dr', 'uses' => 'RecipientTeamController@deleteRecipient']);
-Route::resource('recipientTeam', 'RecipientTeamController');
-Route::resource('template', 'TemplateController', ['only' => ['store', 'update', 'show']]);
 
-# POST
-Route::post('sms/send', ['as' => 'sendsms', 'uses' => 'SmsController@send']);
-Route::post('sms/resend/{smsactivity}', ['as' => 'resendsms', 'uses' => 'SmsActivityController@resend']);
+Route::group(array('before' => 'auth'), function() {
+	# RESOURCES
+	Route::resource('recipient', 'RecipientController', [ 'except' => ['create'] ]);
+	Route::resource('recipientNumber', 'RecipientNumberController', [ 'only' => ['store','update','destroy'] ]);
+	Route::resource('sms', 'SmsController', [ 'only'  =>  ['index', 'edit', 'store'], ]);
+	Route::resource('team', 'TeamController');
+	Route::resource('user', 'UserController');
+	//Route::post('tag/recipient', ['as' => 'tr', 'uses' => 'RecipientTeamController@tag']);
+	Route::post('delete/recipient', ['as' => 'dr', 'uses' => 'RecipientTeamController@deleteRecipient']);
+	Route::resource('recipientTeam', 'RecipientTeamController');
+	Route::resource('template', 'TemplateController', ['only' => ['store', 'update', 'show']]);
 
-# CONTROLLERS
-Route::controllers(array('auth' => 'Auth\AuthController','password' => 'Auth\PasswordController', ));
+	# POST
+	Route::post('sms/send', ['as' => 'sendsms', 'uses' => 'SmsController@send']);
+	Route::post('sms/resend/{smsactivity}', ['as' => 'resendsms', 'uses' => 'SmsActivityController@resend']);
 
-# GET
-Route::get('/', ['as' => '/home', 'uses' => 'SmsController@inbox']);
-Route::get('contacts', ['as'  => 'pb', 'uses' => function() {return view('contacts.show');}]);
-Route::get('groups', ['as' => 'grp', 'uses' => function() {return view('groups.show');}]);
-Route::get('test', function() {return view('tests.testform');});
-Route::get('messaging', ['as' => 'msg', 'uses' => 'SmsController@retSmsCount']);
-Route::get('template/{template_id}', ['as' => 'req_temp', 'uses' => 'TemplateController@show']);
-Route::any('stats/sms', ['uses' => 'StatsController@dailySms']);
-Route::any('sms/{sms}/views', ['uses' => 'SmsController@views']);
-Route::get('sms/inbox', ['uses' => 'SmsController@inbox']);
-Route::get('sms/outbox', ['uses' => 'SmsController@outbox']);
-Route::get('sms/sent', ['uses' => 'SmsController@sent']);
-Route::get('sms/failed', ['uses' => 'SmsController@failed']);
-Route::get('fetch/users', ['as' => 'users', 'uses' => 'UserController@fetchUser']);
-Route::get('fetch/status/{user_id}', ['as' => 'fetchStatus', 'uses' => 'UserController@fetch_status']);
+	# CONTROLLERS
+	Route::controllers(array('auth' => 'Auth\AuthController','password' => 'Auth\PasswordController', ));
 
-# POST
-Route::post('team/{id}/untag', ['as' => 'untag', 'uses' => 'TeamController@untagRecipient']);
+	# GET
+	Route::get('/', ['as' => '/home', 'uses' => 'SmsController@inbox']);
+	Route::get('contacts', ['as'  => 'pb', 'uses' => function() {return view('contacts.show');}]);
+	Route::get('groups', ['as' => 'grp', 'uses' => function() {return view('groups.show');}]);
+	Route::get('test', function() {return view('tests.testform');});
+	Route::get('messaging', ['as' => 'msg', 'uses' => 'SmsController@retSmsCount']);
+	Route::get('template/{template_id}', ['as' => 'req_temp', 'uses' => 'TemplateController@show']);
+	Route::any('stats/sms', ['uses' => 'StatsController@dailySms']);
+	Route::any('sms/{sms}/views', ['uses' => 'SmsController@views']);
+	Route::get('sms/inbox', ['uses' => 'SmsController@inbox']);
+	Route::get('sms/outbox', ['uses' => 'SmsController@outbox']);
+	Route::get('sms/sent', ['uses' => 'SmsController@sent']);
+	Route::get('sms/failed', ['uses' => 'SmsController@failed']);
+	Route::get('fetch/users', ['as' => 'users', 'uses' => 'UserController@fetchUser']);
+	Route::get('fetch/status/{user_id}', ['as' => 'fetchStatus', 'uses' => 'UserController@fetch_status']);
 
-# JSON
-Route::get('getNum', 'RecipientNumberController@getNumber');
-Route::get('retrieve/contacts', 'SmsController@retrieveSms');
-Route::get('retrieve/recipients','SmsController@retrieveRecipient');
-Route::get('sent_sms', 'SmsController@getSent');
+	# POST
+	Route::post('team/{id}/untag', ['as' => 'untag', 'uses' => 'TeamController@untagRecipient']);
 
-Route::get('recipients/json', 'RecipientController@getAllRecipientsJSON');
-Route::get('mobile-numbers/json', 'RecipientNumberController@getAllRecipientNumbersJSON');
-Route::get('teams/json', 'TeamController@getAllTeamsJSON');
-Route::get('sms/status/{status}', 'SmsController@getSmsByStatusJSON');
+	# JSON
+	Route::get('getNum', 'RecipientNumberController@getNumber');
+	Route::get('retrieve/contacts', 'SmsController@retrieveSms');
+	Route::get('retrieve/recipients','SmsController@retrieveRecipient');
+	Route::get('sent_sms', 'SmsController@getSent');
+
+	Route::get('recipients/json', 'RecipientController@getAllRecipientsJSON');
+	Route::get('mobile-numbers/json', 'RecipientNumberController@getAllRecipientNumbersJSON');
+	Route::get('teams/json', 'TeamController@getAllTeamsJSON');
+	Route::get('sms/status/{status}', 'SmsController@getSmsByStatusJSON');
+});
--- resources/views/admin/history.blade.php
+++ resources/views/admin/history.blade.php
@@ -5,5 +5,93 @@
 @stop
 
 @section('content')
-	
+<div class="container">
+	@include('util.m-sidebar')
+	<div class="col-lg-9 col-md-offset-center-2">
+	<br/>
+		<div class="panel panel-default col-lg-12">
+			<div class="page-header">
+				<h3><span class="glyphicon glyphicon-user"></span> History</h3>
+			</div>
+			<br/>
+			<table id="history" class="table"></table>
+			<br/><br/>
+		</div>
+	</div>
+</div>
+@stop
+
+@section('script')
+<script type="text/javascript">
+	$.getJSON("{{ route('audit') }}", function(data) {
+		$('#history').dataTable({
+			"aaData": data,
+			"aaSorting": [],
+			"oLanguage": {
+				"sEmptyTable": "There are currently no User registered...",
+				"sLengthMenu": "per User _MENU_",
+				"oPaginate": {
+					"sFirst": "First ", // This is the link to the first
+					"sPrevious": "&#8592; Previous", // This is the link to the previous
+					"sNext": "Next &#8594;", // This is the link to the next
+					"sLast": "Last " // This is the link to the last
+				}
+			},
+			//DISPLAYS THE VALUE
+			"aoColumns":
+			[
+				{"sTitle": "#", "mDataProp": "user_id"},
+				{"sTitle": "User", "mDataProp": "user_name"},
+				{"sTitle": "Type", "mDataProp": "user_type"},
+				{"sTitle": "E-mail", "mDataProp": "user_email"},
+				{"sTitle": "Status", "mDataProp": "user_status"},
+				{"sTitle": "Recent Update", "mDataProp": "updated_at"},
+			],
+
+			"aoColumnDefs":
+			[
+				//FORMAT THE VALUES THAT IS DISPLAYED ON mDataProp
+				{
+					"aTargets": [ 0 ], // Column to target
+					"mRender": function ( data, type, full ) {
+						return "<label>USER-" + data + "</label>";
+					}
+				},
+				{
+					"aTargets": [ 1 ], // Column to target
+					"mRender": function ( data, type, full ) {
+						var url = '{{ route('user.show', ":id") }}';
+						url = url.replace(':id', full["user_id"]);
+						return "<a href='"+ url +"' class='size-14 text-left'>" + data + "</a>";
+					}
+				},
+				{
+					"aTargets": [ 2 ], // Column to target
+					"mRender": function ( data, type, full ) {
+						return '<label class="size-14 text-left"> ' + data + ' </label>';
+					}
+				},
+				{
+					"aTargets": [ 3 ], // Column to target
+					"mRender": function ( data, type, full ) {
+						return '<label class="text-center size-14"> ' + data + ' </label>';
+					}
+				},
+				{
+					"aTargets": [ 4 ], // Column to target
+					"mRender": function ( data, type, full ) {
+						return '<label class="text-center size-14"> ' + data + ' </label>';
+					}
+				},
+				{
+					"aTargets": [ 5 ], // Column to target
+					"mRender": function ( data, type, full ) {
+						return '<label class="text-center size-14"> ' + data + ' </label>';
+					}
+				}
+			]
+		});
+		$('div.dataTables_filter input').attr('placeholder', 'Filter User');
+	});
+</script>
 @stop
