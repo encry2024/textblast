adec04c4c949732cc3260cf7a8d0b7e52921e584
Lamberto Guevarra <seoblogger07@gmail.com>
2015-06-22 21:16:04 +0800

    add sms view page route

--- app/Http/routes.php
+++ app/Http/routes.php
@@ -59,6 +59,7 @@ Route::get('sms/inbox', ['uses' => 'SmsController@inbox']);
 Route::get('sms/outbox', ['uses' => 'SmsController@outbox']);
 Route::get('sms/sent', ['uses' => 'SmsController@sent']);
 Route::get('sms/failed', ['uses' => 'SmsController@failed']);
+Route::any('sms/{sms}/received', ['uses' => 'SmsController@received']);
 Route::get('fetch/users', ['as' => 'users', 'uses' => 'UserController@fetchUser']);
 Route::get('fetch/status/{user_id}', ['as' => 'fetchStatus', 'uses' => 'UserController@fetch_status']);
 Route::get('fetchHistory', ['as' => 'get_history', 'uses' => 'AuditController@fetchHistory']);
