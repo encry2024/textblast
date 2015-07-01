8bc3be8160bb8d91d03336fc0634d961eab2ff1d
Jake <christan.jake2025@yahoo.com>
2015-07-01 20:20:11 +0800

    Added relationship to User & subjects.

--- app/Activity.php
+++ app/Activity.php
@@ -1,8 +1,8 @@
 <?php namespace App;
 
-use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\Model as Eloquent;
 
-class Activity extends Model {
+class Activity extends Eloquent {
 
 	//
 
