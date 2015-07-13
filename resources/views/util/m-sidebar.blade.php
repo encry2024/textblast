f5d09a20a809251e80af5debbe3e65b1339e5ec8
Jake <christan.jake2025@yahoo.com>
2015-07-13 19:47:20 +0800

    removed left class in html after panel

--- resources/views/util/m-sidebar.blade.php
+++ resources/views/util/m-sidebar.blade.php
@@ -1,7 +1,7 @@
 <div class="col-lg-3">
     <br/>
     <div class="panel panel-default col-lg-12">
-        <div class="panel-body left col-lg-12">
+        <div class="panel-body col-lg-12">
             <a href="{{ route('msg') }}"><span class="glyphicon glyphicon-comment"></span> Send SMS </a>
             <br/><br/>
             <a href="{{ url('stats/sms')  }}"><span class="glyphicon glyphicon-stats"></span> Stats </a>
