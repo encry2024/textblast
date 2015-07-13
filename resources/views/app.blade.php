07c7e6a4be9d4b3b8b501d3730c536ec5066b4df
Jake <christan.jake2025@yahoo.com>
2015-07-13 20:58:04 +0800

    Changed Build App to the last commit in github

--- resources/views/app.blade.php
+++ resources/views/app.blade.php
@@ -48,7 +48,7 @@
 		<div class="col-lg-12">
 			<hr/>
 			<label class="size-12 app-info-label" for=""><span class="">© 2015 Northstar Solutions, Inc.</span></label>
-			<label class="right size-12 app-info-label" for="">Textblast v1.0 &mdash; Build App <kbd>66718f3</kbd></label>
+			<label class="right size-12 app-info-label" for="">Textblast v1.0 &mdash; Build App <kbd>8a461f2</kbd></label>
 		</div>
 	</div>
 	<style>
