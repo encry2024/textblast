b75f1b7c406e2edc5223a39fb5734217979d2dbc
Lamberto Guevarra <seoblogger07@gmail.com>
2015-07-16 14:26:40 +0800

    removed sms resend command

--- app/Console/Kernel.php
+++ app/Console/Kernel.php
@@ -26,8 +26,8 @@ class Kernel extends ConsoleKernel {
 	{
 		$schedule->command('inspire')
 				 ->hourly();
-		$schedule->command('sms:resend')
-				->everyTenMinutes();
+		/*$schedule->command('sms:resend')
+				->everyTenMinutes();*/
 	}
 
 }
